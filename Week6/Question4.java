33b9b1930c31fc38c667d4e24874beb0f308ebd7
JCode957 <jaisaketh2016@gmail.com>
2026-09-12 11:51:19 +0530

    Library ID Card Management

---
+++ Week6/Question4.java
@@ -0,0 +1,28 @@
+class Question4
+{
+    static class IdCard
+    {
+        String name;
+        int booksIssued;
+
+        IdCard(String n,int b)
+        {
+            name=n;
+            booksIssued=b;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        IdCard ravi=new IdCard("Ravi",0);
+        IdCard duplicate=ravi;
+
+        duplicate.booksIssued=3;
+
+        IdCard separate=new IdCard("Ravi",3);
+
+        System.out.println("Ravi's booksIssued: "+ravi.booksIssued);
+        System.out.println("duplicate == ravi: "+(duplicate==ravi));
+        System.out.println("separate == ravi: "+(separate==ravi));
+    }
+}
